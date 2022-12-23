6f6b7bd66a2596c3342428a37cd1eb084d41a1d2
Haruka <hfujii1013@gmail.com>
2022-12-22 20:21:03 -0800

    first commit

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ var numberOfDrumButons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!");
-       
-        //what to do when click detected.
+        // alert("I got clicked!");
+    
+       console.log(this);
+
+        
        });
        
 }
+
+//addEventListener(①, ②)
+        //①どのイベントをリスニングすべきかを指定
+        //②そのイベントが検出された時に何をすべきかを指定
+        //what to do when click detected.
+
+// var audio = new Audio("sounds/tom-1.mp3")
+//         audio.play();
 
 
 
